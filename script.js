7cb9b4560ec1dd470349b4be5a84bacf7ad54302
Akinola Ayobami <ayobamiakinola96@gmail.com>
2026-05-20 04:45:58 +0100

    More details inclusive

---
+++ script.js
@@ -0,0 +1,9 @@
+// var typed = Typed(".typing",{
+//     strings:["Software Engineer","Web developer","Web designer","Hardware Engineer", "Graphics Designer"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
+
+
+  
