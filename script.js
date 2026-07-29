b636cb02f0f07ff3db88c5365fdabce6bd4335f5
Akinola Ayobami <ayobamiakinola96@gmail.com>
2026-07-30 00:14:36 +0100

    Responsiveness 80%

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 //     BackSpeed:60,
 //     loop:true
 // })
-
 const swiper = new Swiper('.slider-wrapper',{
     // optional parameters
     // direction: 'vertical',
@@ -12,41 +11,46 @@ const swiper = new Swiper('.slider-wrapper',{
     spaceBetween: 30,
     grabCursor: true,
     //if we need pagination
-    pagination: {
-        el: '.swiper-pagination',
+    pagination:{
+        el:'.swiper-pagination',
         clickable:true,
         DynamicsCompressorNode:true,
         dynamicBullets:true,
     },
     //Navigation arrows
-    navigation: {
+    navigation:{
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     // And if we need scrollbar
-    scrollbar: {
+    scrollbar:{
         el: '.swiper-scrollbar',
     },
     // Responsive Breakpoints
-    breakpoints: {
+    breakpoints:{
         0: {
             slidesPerView: 1
         },
-
         768: {
             slidesPerView: 2
-        },
- 
+        }, 
         1080: {
-
             slidesPerView: 3
         },
 
          1392: {
-
             slidesPerView: 4
         }
 
     }
 });
-  
+
+  function showSidebar(){
+    const sidebar = document.querySelector(".navbar")
+    sidebar.style.display = "flex"
+  }
+    function hideSidebar(){
+    const sidebar = document.querySelector(".navbar")
+    sidebar.style.display = "none"
+  }
+
